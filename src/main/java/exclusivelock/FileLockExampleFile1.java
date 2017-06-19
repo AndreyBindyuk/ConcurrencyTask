512f5ea7e514a16e5f7b5942b75844d8b48bc2c0
Andrey <anbind@mail.ru>
2017-06-20 01:09:11 +0600

    Concurrency task init commit

---
+++ src/main/java/exclusivelock/FileLockExampleFile1.java
@@ -0,0 +1,35 @@
+package exclusivelock;
+
+import sun.security.action.GetPropertyAction;
+
+import static java.security.AccessController.doPrivileged;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+
+public class FileLockExampleFile1 {
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
+        Path tempDir = Paths.get(doPrivileged(new GetPropertyAction(
+                "java.io.tmpdir")));
+        File file = new File(tempDir.toAbsolutePath() + File.separator
+                + "testLocking.txt");
+        if (file.exists())
+            file.delete();
+        file.createNewFile();
+        FileChannel fileChannel = new RandomAccessFile(file, "rw").getChannel();
+        FileLock lock = fileChannel.lock();
+        fileChannel
+                .write(Charset.defaultCharset().encode(CharBuffer.wrap("a")));
+        fileChannel.force(false);
+        Thread.sleep(20000);
+        lock.release();
+
+    }
+}
---
+++ src/main/java/exclusivelock/FileLockExampleFile2.java
@@ -0,0 +1,29 @@
+package exclusivelock;
+
+import sun.security.action.GetPropertyAction;
+
+import static java.security.AccessController.doPrivileged;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.*;
+
+public class FileLockExampleFile2 {
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
+        Path tempDir = Paths.get(doPrivileged(new GetPropertyAction(
+                "java.io.tmpdir")));
+        File file = new File(tempDir.toAbsolutePath() + File.separator
+                + "testLocking.txt");
+        RandomAccessFile rf = new RandomAccessFile(file, "rw");
+        FileChannel fileChannel = rf.getChannel();
+        System.out.println("Trying to acquire lock");
+        FileLock lock = fileChannel.lock();
+        System.out.println("file lock acquired");
+        lock.release();
+
+    }
+}
